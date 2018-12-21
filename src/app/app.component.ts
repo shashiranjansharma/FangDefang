6cab451af1c11d771dd94d642372dddb73748070
shashiranjansharma <shashiranjansharma@live.in>
2018-12-21 13:04:01 +0530

    Making navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,31 @@ export class AppComponent {
 
 	}
 
-	abcd = this.data.links;
+	my_data = this.data.links;
 	i = null;
-  
-	displayChild = false;
-	displaySubChild = false;
-	displaySubSubChild = false;
+	my_child1 = [];
+	my_child2 = [];
+	my_child3 = [];
+	child1_top = 0;
+	child1_left = 0;
+	child2_top = 0;
+	child2_left = 0;
+	child3_top = 0;
+	child3_left = 0;
 
-	getLink(event, links, i){
-		console.log(links.links);
-		if(links.links.length != 0 && links.links[0].childLinks == true){
-			this.displayChild = true;
-			this.i = i;
-			console.log(this.displayChild +" "+ i);
- 		}
- 		else if((links.links.length != 0) && (links.links[0].subChildLinks == true)){
-			this.displaySubChild = true;
- 		}
- 		else if((links.links.length != 0) && (links.links[0].subSubChildLinks == true)){
-			this.displaySubSubChild = true;
- 		}
- 		else{
- 		 	this.displayChild = false;
- 		}
-		
+	getChild1(event, link, index){
+		this.my_child1 = this.my_data[index].links;
+		this.child1_left = event.clientX;
+		this.child1_top = event.clientY;
 	}
-
+	getChild2(event, link, index){
+	    this.my_child2 = this.my_child1[index].links;
+	    this.child2_left = event.clientX;
+		this.child2_top = event.clientY;
+	}
+	getChild3(event, link, index){
+	    this.my_child3 = this.my_child2[index].links;
+	    this.child3_left = event.clientX;
+		this.child3_top = event.clientY;
+	}
 }
